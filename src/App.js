d65babff782c4ff27a786808e33399b75af62826
Joaquin <jiaraya4@uc.cl>
2021-05-17 03:51:51 -0400

    socket connected and listeners added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import io from "socket.io-client";
+
+// const socket = io("wss://tarea-3-websocket.2021-1.tallerdeintegracion.cl/", {
+//   transports: ['websocket']  // forces websockets only
+// });
+
+const socket = io("wss://tarea-3-websocket.2021-1.tallerdeintegracion.cl", {path: '/flights'});
+
+socket.on("connect", () => {
+  console.log(socket.connected); // true
+});
+
+// socket.on("POSITION", (...args) => {
+//   console.log(args[0], "POSITION")
+// });
+socket.on("FLIGHTS", (...args) => {
+  console.log(args)
+});
+socket.on("CHAT", (...args) => {
+  console.log('CHAT')
+});
+socket.emit("FLIGHTS")
+
 function App() {
   return (
     <div className="App">
