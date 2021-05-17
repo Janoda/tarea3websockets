6854cdb4b306dc0ad57fa5241bd084f601530c86
Joaquin <jiaraya4@uc.cl>
2021-05-17 04:56:09 -0400

    added react leaflet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 import io from "socket.io-client";
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
 
 // const socket = io("wss://tarea-3-websocket.2021-1.tallerdeintegracion.cl/", {
 //   transports: ['websocket']  // forces websockets only
@@ -23,24 +25,22 @@ socket.on("CHAT", (...args) => {
   console.log('CHAT')
 });
 socket.emit("FLIGHTS")
-
+const position = [51.505, -0.09]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map center={position} zoom={2} scrollWheelZoom={false} style={{ height: '100vh', width: '100wh' }}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+
     </div>
   );
 }
